1969f49faea1903097451cd881752b56172003c8
Rashidat <rawsheedah@yahoo.com>
2024-08-05 13:11:46 +0100

    generate pre-configured spring boot initializr file with download

---
+++ src/main/java/com/cbfacademy/restapiexercise/RestApiExerciseApplication.java
@@ -0,0 +1,17 @@
+package com.cbfacademy.restapiexercise;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiExerciseApplication.class, args);
+	}
+
+}
